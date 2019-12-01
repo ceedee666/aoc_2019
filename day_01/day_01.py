8274f397c0d64fb869a6abb2370772b14e341846
Christian Drumm <drumm@fh-aachen.de>
2019-12-01 20:05:18 +0100

    Solution for day 1

---
+++ day_01/day_01.py
@@ -0,0 +1,21 @@
+def calculate_fuel(mass):
+    return int(mass/3) - 2
+
+def calculate_fuel_for_modules(modules):
+    return sum(map(calculate_fuel, modules))
+
+def calculate_total_fuel(modules):
+    all_masses = []
+    for mass in modules:
+        mass = calculate_fuel(mass)
+        while mass > 0:
+            all_masses.append(mass)
+            mass = calculate_fuel(mass)
+    return sum(all_masses)
+
+if __name__ == "__main__":
+    with open("day_01/input_01.txt") as f:
+        modules = list(map(int,f.readlines()))
+
+    print("Fuel for modules (Puzzle 1):", calculate_fuel_for_modules(modules))
+    print("Total fuel (Puzzle 2):", calculate_total_fuel(modules))
