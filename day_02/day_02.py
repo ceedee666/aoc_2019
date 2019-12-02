5d92161cb8da8921ae9717598df2f4665f7345c6
Christian Drumm <drumm@fh-aachen.de>
2019-12-02 10:28:45 +0100

    Solution of part 1. After fixing the input data...

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,9 +1,24 @@
+HCF = 99
+ADD = 1
+
 def execute_intcode(program):
-    return []
+    instruction_pointer = 0
+    while program[instruction_pointer] != HCF:
+        operand_1_address = program[instruction_pointer + 1]
+        operand_2_address = program[instruction_pointer + 2]
+        target_address = program[instruction_pointer + 3]
+
+        if program[instruction_pointer] == ADD:
+            program[target_address] = program[operand_1_address] + program[operand_2_address]
+        else:
+            program[target_address] = program[operand_1_address] * program[operand_2_address]
+        
+        instruction_pointer += 4
+    return program
 
 if __name__ == "__main__":
-    with open("2/input_02.txt") as f:
-        program = f.readline().split(',')
+    with open("day_02/input_02.txt") as f:
+        program = list(map(int,f.readline().split(',')))
         result = execute_intcode(program)
 
         print("Result for part 1:", result[0])
--- day_02/day_02_test.py
+++ day_02/day_02_test.py
@@ -1,8 +1,9 @@
 import pytest
-from day_02 import *
+import day_02
 
 def test_execute_intcode():
-    assert execute_intcode([1,0,0,0,99]) == [2,0,0,0,99]
-    assert execute_intcode([2,3,0,3,99]) == [2,3,0,6,99]
-    assert execute_intcode([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
-    assert execute_intcode([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+    assert day_02.execute_intcode([1,0,0,0,99]) == [2,0,0,0,99]
+    assert day_02.execute_intcode([2,3,0,3,99]) == [2,3,0,6,99]
+    assert day_02.execute_intcode([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
+    assert day_02.execute_intcode([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+    assert day_02.execute_intcode([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
