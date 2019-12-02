7516d937fc962b0b367dff9ec36e1a2af947f824
Christian Drumm <drumm@fh-aachen.de>
2019-12-02 11:02:35 +0100

    Solution Part 2

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,5 +1,6 @@
 HCF = 99
 ADD = 1
+PART2_MAGIC_NUMBER = 19690720
 
 def execute_intcode(program):
     instruction_pointer = 0
@@ -7,7 +8,7 @@ def execute_intcode(program):
         operand_1_address = program[instruction_pointer + 1]
         operand_2_address = program[instruction_pointer + 2]
         target_address = program[instruction_pointer + 3]
-
+        
         if program[instruction_pointer] == ADD:
             program[target_address] = program[operand_1_address] + program[operand_2_address]
         else:
@@ -16,9 +17,30 @@ def execute_intcode(program):
         instruction_pointer += 4
     return program
 
+def calculate_noun_and_verb(program):
+    for noun in range(100):
+        for verb in range(100):
+            new_program = list(program)
+            new_program[1] = noun
+            new_program[2] = verb
+
+            try:
+                result = execute_intcode(new_program)
+                if result[0] == PART2_MAGIC_NUMBER:
+                    return noun,verb
+            except IndexError as e:
+                pass
+
+
 if __name__ == "__main__":
     with open("day_02/input_02.txt") as f:
-        program = list(map(int,f.readline().split(',')))
-        result = execute_intcode(program)
+        input = list(map(int,f.readline().split(',')))
 
-        print("Result for part 1:", result[0])
+    program_part_1 = list(input)
+    program_part_1[1] = 12
+    program_part_1[2] = 2
+    result = execute_intcode(program_part_1)
+    print("Result for part 1:", result[0])
+
+    noun,verb = calculate_noun_and_verb(input)
+    print("Result for part 2:", 100 * noun + verb)
