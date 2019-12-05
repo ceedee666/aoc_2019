33a8a662e55d6c76dd3913742712aeddf9b0b5e8
Christian Drumm <drumm@fh-aachen.de>
2019-12-05 15:47:21 +0100

    part two finished

--- day_05/day_05.py
+++ day_05/day_05.py
@@ -1,16 +1,18 @@
-
 OPCODES = { 
-    "ADD"       : 1,
-    "MULT"      : 2,
-    "INPUT"     : 3,
-    "OUTPUT"    : 4, 
-    "HCF"       : 99 }
+    "ADD" : 1,
+    "MUL" : 2,
+    "INP" : 3,
+    "OUT" : 4,
+    "JIT" : 5,
+    "JIF" : 6 ,
+    "LES" : 7,
+    "EQU" : 8,
+    "HCF" : 99 }
 
 PARA_MODE = {
     'POS' : 0,
     'IMM' : 1
 }    
-
 
 def execute_intcode(program, input=0):
     instruction_pointer = 0
@@ -22,31 +24,70 @@ def execute_intcode(program, input=0):
         if opcode_details[0] == OPCODES["HCF"]:
             hcf = True
         else:
-            operand_1_address = calc_operand_address(instruction_pointer + 1, opcode_details[1], program)
-            operand_2_address = calc_operand_address(instruction_pointer + 2, opcode_details[2], program)
-            target_address = calc_operand_address(instruction_pointer + 3, opcode_details[3], program)
-            
+            operand_1_address, operand_2_address, operand_3_address = calc_operand_addresses(instruction_pointer, opcode_details, program)
+            new_instruction_pointer = instruction_pointer
+
             if opcode_details[0] == OPCODES["ADD"]:
-                program[target_address] = program[operand_1_address] + program[operand_2_address]
-            elif opcode_details[0] == OPCODES["MULT"]:
-                program[target_address] = program[operand_1_address] * program[operand_2_address]
-            elif opcode_details[0] == OPCODES["INPUT"]:
+                program[operand_3_address] = program[operand_1_address] + program[operand_2_address]
+
+            elif opcode_details[0] == OPCODES["MUL"]:
+                program[operand_3_address] = program[operand_1_address] * program[operand_2_address]
+
+            elif opcode_details[0] == OPCODES["INP"]:
                 program[operand_1_address] = input
-            elif opcode_details[0] == OPCODES["OUTPUT"]:
+
+            elif opcode_details[0] == OPCODES["OUT"]:
                 output.append(program[operand_1_address])
 
-            instruction_pointer = calc_next_ip(instruction_pointer, opcode_details[0])
+            elif opcode_details[0] == OPCODES["JIT"]:
+                if program[operand_1_address] != 0:
+                    new_instruction_pointer = program[operand_2_address]
+
+            elif opcode_details[0] == OPCODES["JIF"]:
+                if program[operand_1_address] == 0:
+                    new_instruction_pointer = program[operand_2_address]
+
+            elif opcode_details[0] == OPCODES["LES"]:
+                if program[operand_1_address] < program[operand_2_address]:
+                    program[operand_3_address] = 1
+                else:
+                    program[operand_3_address] = 0
+
+            elif opcode_details[0] == OPCODES["EQU"]:
+                if program[operand_1_address] == program[operand_2_address]:
+                    program[operand_3_address] = 1
+                else:
+                    program[operand_3_address] = 0 
+
+            if instruction_pointer == new_instruction_pointer:
+                instruction_pointer = calc_next_ip(instruction_pointer, opcode_details[0])
+            else:
+                instruction_pointer = new_instruction_pointer
     return program, output
 
 def calc_next_ip(instruction_pointer, opcode):
-    if opcode == OPCODES["OUTPUT"] or opcode == OPCODES["INPUT"]:
+    if opcode in (OPCODES["INP"], OPCODES["OUT"]):
         instruction_pointer += 2
+    elif opcode in (OPCODES["JIT"], OPCODES["JIF"]):
+        instruction_pointer += 3
     else:
         instruction_pointer += 4
     return instruction_pointer
 
 def calc_operand_address(instruction_pointer, mode, program ):
-    return instruction_pointer if mode == PARA_MODE['IMM'] else program[instruction_pointer]
+    operand_address = 0 
+    if mode == PARA_MODE['IMM']:
+        operand_address = instruction_pointer 
+    else:        
+        if instruction_pointer < len(program):
+            operand_address = program[instruction_pointer] 
+    return operand_address 
+
+def calc_operand_addresses(instruction_pointer, opcode_details, program ):
+    operand_1_address = calc_operand_address(instruction_pointer + 1, opcode_details[1], program)
+    operand_2_address = calc_operand_address(instruction_pointer + 2, opcode_details[2], program)
+    operand_3_address = calc_operand_address(instruction_pointer + 3, opcode_details[3], program)
+    return operand_1_address, operand_2_address, operand_3_address
 
 def analyse_opcode(opcode):
     opcode_list = list(str(opcode))
@@ -57,8 +98,11 @@ def analyse_opcode(opcode):
 def pad_opcode(opcode_list):
     return (5 - len(opcode_list)) * ["0"] + opcode_list
 
+
 if __name__ == "__main__":
     with open("day_05/input.txt") as f:
-        program = list(map(int,f.readline().split(',')))
-        
-    print("Diagnostics code for module 1 (air condition unit): ", execute_intcode(program, 1)[1][-1])
+        program_part_1 = list(map(int,f.readline().split(',')))
+        program_part_2 = list(program_part_1)
+
+    print("Diagnostics code for module 1 (air condition unit): ", execute_intcode(program_part_1, 1)[1][-1])
+    print("Diagnostics code for module 5 (thermal radiators): ", execute_intcode(program_part_2, 5)[1][-1])
--- day_05/day_05_test.py
+++ day_05/day_05_test.py
@@ -1,17 +1,53 @@
 import pytest
 import day_05
 
-def test_execute_intcode():
-    # Day 2 tests
+def test_execute_intcode_day_2():
     assert day_05.execute_intcode([1,0,0,0,99])[0] == [2,0,0,0,99]
     assert day_05.execute_intcode([2,3,0,3,99])[0] == [2,3,0,6,99]
     assert day_05.execute_intcode([2,4,4,5,99,0])[0] == [2,4,4,5,99,9801]
     assert day_05.execute_intcode([1,1,1,4,99,5,6,0,99])[0] == [30,1,1,4,2,5,6,0,99]
     assert day_05.execute_intcode([1,9,10,3,2,3,11,0,99,30,40,50])[0] == [3500,9,10,70,2,3,11,0,99,30,40,50]
 
-    # Day 5 tests
+def test_execute_intcode_day_5():
+    # Part 1
     assert day_05.execute_intcode([1002,4,3,4,33])[0] == [1002,4,3,4,99]
     assert day_05.execute_intcode([1101,100,-1,4,0])[0] == [1101,100,-1,4,99]
+
+    # Part 2
+    # Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not)
+    assert day_05.execute_intcode([3,9,8,9,10,9,4,9,99,-1,8], 8)[1] == [1]
+    assert day_05.execute_intcode([3,9,8,9,10,9,4,9,99,-1,8], 9)[1] == [0]
+
+    #Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not)
+    assert day_05.execute_intcode([3,9,7,9,10,9,4,9,99,-1,8], 7)[1] == [1]
+    assert day_05.execute_intcode([3,9,7,9,10,9,4,9,99,-1,8], 9)[1] == [0]
+
+    #Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+    assert day_05.execute_intcode([3,3,1108,-1,8,3,4,3,99], 8)[1] == [1]
+    assert day_05.execute_intcode([3,3,1108,-1,8,3,4,3,99], 5)[1] == [0]
+    #Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+    assert day_05.execute_intcode([3,3,1107,-1,8,3,4,3,99], 7)[1] == [1]
+    assert day_05.execute_intcode([3,3,1107,-1,8,3,4,3,99], 9)[1] == [0]
+
+    # Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+    assert day_05.execute_intcode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0)[1] == [0]
+    assert day_05.execute_intcode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 5)[1] == [1] 
+    assert day_05.execute_intcode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0)[1] == [0]  
+    assert day_05.execute_intcode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 5)[1] == [1]
+
+    # Here's a larger example:
+    # The program uses an input instruction to ask for a single number.
+    # The program will then output 999 if the input value is below 8, 
+    # output 1000 if the input value is equal to 8, 
+    # or output 1001 if the input value is greater than 8.
+    example_program = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+
+    assert day_05.execute_intcode(example_program, 7)[1] == [999]
+    assert day_05.execute_intcode(example_program, 8)[1] == [1000]
+    assert day_05.execute_intcode(example_program, 9)[1] == [1001] 
+
+
+
 
 def test_analyse_opcode():
     assert day_05.analyse_opcode(1002) == (2,0,1,0)
@@ -21,6 +57,8 @@ def test_calc_operand_address():
     assert day_05.calc_operand_address(1, day_05.PARA_MODE['IMM'], [4,3,2,1]) == 1
 
 def test_calc_next_ip():
-    assert day_05.calc_next_ip(0, day_05.OPCODES['INPUT']) == 2
-    assert day_05.calc_next_ip(0, day_05.OPCODES['OUTPUT']) == 2
+    assert day_05.calc_next_ip(0, day_05.OPCODES['JIF']) == 3
+    assert day_05.calc_next_ip(0, day_05.OPCODES['JIT']) == 3
+    assert day_05.calc_next_ip(0, day_05.OPCODES['INP']) == 2
+    assert day_05.calc_next_ip(0, day_05.OPCODES['OUT']) == 2
     assert day_05.calc_next_ip(0, day_05.OPCODES['ADD']) == 4
